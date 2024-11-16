30ae2e939f801bbeb4a46cb997368c47d750e9f0
Nicolas A. Romero <nicolas.romeroc@hotmail.com>
2024-11-16 01:02:38 -0500

    Se añadio un html nuevo, para controlar la redireccion del usuario cuando "ingresaba" correctamente, en ella se añadieron eventos y se incluyeron tambien en el app.js, para crear un sistema de cierre de sesion y se puso un metodo de seguridad para evitar el reingreso del usuario con las "flechas" de volver o retroceder del navegador, se incluyo el CSS de esta pestaña nueva, queda listo para deploy.

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const bodyParser=require('body-parser')
 const path=require('path')
 const mysql=require('mysql2/promise')
 const session=require('express-session')
-const { connect } = require('http2')
 const app=express()
+app.use(session({
+    secret:'Secreto',
+    resave:false,
+    saveUninitialized: true,
+}))
 //Middleware
 app.use(bodyParser.urlencoded({extends:true}))
 app.use(bodyParser.json())
@@ -20,41 +24,74 @@ const db={
 
 app.post('/iniciar',async (req,res)=>{
     const {correo, pass}=req.body
-    const conect= await mysql.createConnection(db)
-    const [row]= await conect.execute('SELECT * FROM sena WHERE Correo=? AND Pass=?', [correo,pass])
-    console.log(row)
-    if(row.length>0){
-        res.status(201).send("INGRESASTE A NUESTRA INTRANET")
-    }else{
-        res.status(401).send(`
-            <script>
-                window.onload=function(){
-                    alert('Usuario no Registrado')
-                    window.location.href='/registro.html'
-                }
-            </script>
-            `)
+    try{
+        const conect= await mysql.createConnection(db)
+        const [row]= await conect.execute('SELECT * FROM sena WHERE Correo=? AND Pass=?', [correo,pass])
+        console.log(row)
+
+        if(row.length>0){
+            req.session.Nombre=row[0].Nombre
+            req.session.nombre_Usuario=row[0].Nombre
+            res.locals.nombre_Usuario=row[0].Nombre
+            res.sendFile(path.join(__dirname,'usuario.html'))
+        }else{
+            res.status(401).send(`
+                <script>
+                    window.onload=function(){
+                        alert('Usuario no Registrado')
+                        window.location.href='/registro.html'
+                    }
+                </script>
+                `)
+        }
+        await conect.end()
     }
-    await conect.end()
+    catch(err){
+        console.error(err)
+    }
 })
 app.post('/registro',async (req,res)=>{
     const {correo,nombre,tipoID,documento,pass}=req.body
-    console.log(correo,nombre,tipoID,documento,pass)
-    const conect= await mysql.createConnection(db)
-    const [row]= await conect.execute(' INSERT INTO sena (Correo, Nombre, TipoID, Documento, Pass) VALUES (?,?,?,?,?)', [correo,nombre,tipoID,documento,pass])
-    if(row.length<0){
-        res.status(401).send("Registra un usuario valido")
-    }else{
-        res.status(201).send(`
-            <script>
-                window.onload=function(){
-                    alert('El usuario ha sido registrado Exitosamente')
-                    window.location.href='/iniciar.html'
-                }
-            </script>
-            `)
+    try{
+        console.log(correo,nombre,tipoID,documento,pass)
+        const conect= await mysql.createConnection(db)
+        const [row]= await conect.execute(' INSERT INTO sena (Correo, Nombre, TipoID, Documento, Pass) VALUES (?,?,?,?,?)', [correo,nombre,tipoID,documento,pass])
+        if(row.length<0){
+            res.status(401).send("Registra un usuario valido")
+        }else{
+            res.status(201).send(`
+                <script>
+                    window.onload=function(){
+                        alert('El usuario ha sido registrado Exitosamente')
+                        window.location.href='/iniciar.html'
+                    }
+                </script>
+                `)
+        }
+        await conect.end()
     }
-    await conect.end()
+    catch(err){
+        console.error(err)
+    }
+})
+
+app.get('/obtener-usuario',(req,res)=>{
+    const usuario=req.session.nombre_Usuario
+    if(usuario){
+        res.json({nombre:usuario})
+    }else{
+        res.status(401).send("No se encontro el nombre")
+    }
+})
+app.post('/cerrar', (req,res)=>{
+    req.session.destroy((err)=>{
+        if(err){
+            console.error('No se pudo')
+            res.status(500).send('Error')
+        }else{
+            res.status(201).send("Cerrado")
+        }
+    })
 })
 
 app.listen(3000, ()=>{
